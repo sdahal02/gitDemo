22c55a416afe9f044e475b699091a3783ab10d8c
sdahal02 <dahalsovita@gmail.com>
2024-08-31 19:26:18 -0500

    Initial project  commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b ;
+        System.out.print("Welcome to Maven project"+ " "+ "Sum of a and b is = "+ sum);
+    }
+}
